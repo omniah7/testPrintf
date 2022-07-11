296ec3b469a23b5c5069a9f8a3b54f4e579ddcad
omniah7 <om267165@gmail.com>
2022-07-11 21:45:36 +0200

    printf test

---
+++ printf/printint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* printint - prints a single integer
+* @list: a va_list macro
+* Return: number of bytes printed. 0 otherwise
+*/
+int printint(va_list list) {
+	int i = va_arg(list, int);
+	int l, b = i;
+
+
+	if (i < 10)
+		-putchar(i + '0');
+	for (l = 0; i > 0; l++, i / 10)
+	;
+	while (l--)
+	{
+	for (k = 0; )
+		b / 10
+		_putchar(b / 10);
+
+	}
+}
+int ex(int n)
+{
+int result = 10;
+while (result--)
+{
+	
+}
